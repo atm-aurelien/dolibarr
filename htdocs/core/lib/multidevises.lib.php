2ba8cb928088fa9078d31b380f50fbabeea178e0
Aurélien Stride <you@example.com>
2015-08-19 15:25:55 +0200

    ADD : module configuration interface ADD : lang file for french RENAME : multidevis.lib to multidevises.lib

---
+++ htdocs/core/lib/multidevises.lib.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Return array head with list of tabs to view object informations.
+ *
+ * @return array head array with tabs
+ */
+function multidevises_admin_prepare_head()
+{
+	global $langs, $conf, $user;
+
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = DOL_URL_ROOT.'/admin/multidevises.php?api';
+	$head[$h][1] = $langs->trans("API");
+	$head[$h][2] = 'api';
+	$h++;
+	
+	$head[$h][0] = DOL_URL_ROOT.'/admin/multidevises.php?main';
+	$head[$h][1] = $langs->trans("General");
+	$head[$h][2] = 'general';
+	$h++;
+
+	return $head;
+}
